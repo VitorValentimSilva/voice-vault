d9b5f8ffa18ae19838e245152f40d31a6765bf8c
Vitor Valentim da Silva Ferreira <vitorvalentin840@gmail.com>
2026-06-23 00:25:24 -0300

    feat: add EnvModule and EnvService for environment configuration management

---
+++ backend/src/config/env/env.service.ts
@@ -0,0 +1,62 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+import { Env } from '@/config/env/dto/env.dto';
+import { NodeEnv } from '@/enum/env.enum';
+
+@Injectable()
+export class EnvService {
+  constructor(private readonly configService: ConfigService<Env, true>) {}
+
+  get nodeEnv() {
+    return this.configService.get('NODE_ENV', {
+      infer: true,
+    });
+  }
+
+  get isDevelopment() {
+    return this.nodeEnv === NodeEnv.DEVELOPMENT;
+  }
+
+  get isTest() {
+    return this.nodeEnv === NodeEnv.TEST;
+  }
+
+  get isProduction() {
+    return this.nodeEnv === NodeEnv.PRODUCTION;
+  }
+
+  get port() {
+    return this.configService.get('PORT', {
+      infer: true,
+    });
+  }
+
+  get databaseUrl() {
+    return this.configService.get('DATABASE_URL', {
+      infer: true,
+    });
+  }
+
+  get sentryDsn() {
+    return this.configService.get('SENTRY_DSN', {
+      infer: true,
+    });
+  }
+
+  get clerkWebhookSecret(): string {
+    return this.configService.getOrThrow<string>('CLERK_WEBHOOK_SECRET');
+  }
+
+  get posthogKey() {
+    return this.configService.get('POSTHOG_KEY', {
+      infer: true,
+    });
+  }
+
+  get posthogHost() {
+    return this.configService.get('POSTHOG_HOST', {
+      infer: true,
+    });
+  }
+}
