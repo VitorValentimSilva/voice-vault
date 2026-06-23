d989224508829db7c14543ec6dd45a37db678669
Vitor Valentim da Silva Ferreira <vitorvalentin840@gmail.com>
2026-06-23 00:25:02 -0300

    feat: add environment configuration interface and validation schema

---
+++ backend/src/config/env/dto/env.dto.ts
@@ -0,0 +1,27 @@
+import * as Joi from 'joi';
+
+import { NodeEnv } from '@/enum/env.enum';
+
+export interface Env {
+  NODE_ENV: NodeEnv;
+  PORT: number;
+  DATABASE_URL: string;
+  SENTRY_DSN: string;
+  CLERK_WEBHOOK_SECRET: string;
+  POSTHOG_KEY: string;
+  POSTHOG_HOST: string;
+}
+
+export const EnvSchema = Joi.object({
+  NODE_ENV: Joi.string()
+    .valid(...Object.values(NodeEnv))
+    .required(),
+  PORT: Joi.number().port().default(3000),
+  DATABASE_URL: Joi.string()
+    .uri({ scheme: ['postgres', 'postgresql'] })
+    .required(),
+  SENTRY_DSN: Joi.string().uri().required(),
+  CLERK_WEBHOOK_SECRET: Joi.string().required(),
+  POSTHOG_KEY: Joi.string().required(),
+  POSTHOG_HOST: Joi.string().uri().required(),
+});
