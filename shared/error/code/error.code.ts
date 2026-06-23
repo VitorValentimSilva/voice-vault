abdc5158ce0fb7faed62df76ddaa0c4e79acebf5
Vitor Valentim da Silva Ferreira <vitorvalentin840@gmail.com>
2026-06-23 19:55:42 -0300

    fix: update ERROR_CODE type definition for improved error handling

--- shared/error/code/error.code.ts
+++ shared/error/code/error.code.ts
@@ -1,11 +1,9 @@
 import { RATE_LIMIT_CODE } from '@/error/code/rate-limit.code';
 import { REDIS_CODE } from '@/error/code/redis.code';
 
-export const ERROR_CODE = {
+export const ERROR_CODE: Record<string, string> = {
   ...REDIS_CODE,
   ...RATE_LIMIT_CODE,
 } as const;
 
-export type ErrorCode =
-  | (typeof RATE_LIMIT_CODE)[keyof typeof RATE_LIMIT_CODE]
-  | (typeof REDIS_CODE)[keyof typeof REDIS_CODE];
+export type ErrorCode = keyof typeof ERROR_CODE;
