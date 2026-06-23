8192658b14ecfa89184e90d2c58e4b79956a2edd
Vitor Valentim da Silva Ferreira <vitorvalentin840@gmail.com>
2026-06-23 19:22:29 -0300

    feat: update ESLint configuration to ignore itself and add new path mappings in tsconfig.json fix: update package dependencies for improved compatibility and security

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,6 +6,7 @@ import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
+  { ignores: ['**/eslint.config.*'] },
   js.configs.recommended,
   eslintPluginPrettierRecommended,
   ...tseslint.configs.recommendedTypeChecked,
