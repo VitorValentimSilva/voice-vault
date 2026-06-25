1351d8bede191ab0ac89aa1ebad42e4a843c5a5e
Vitor Valentim da Silva Ferreira <vitorvalentin840@gmail.com>
2026-06-25 18:13:15 -0300

    ✨ feat(http): add HTTP status codes and integrate into error handling

--- shared/error/code/error.code.ts
+++ shared/error/code/error.code.ts
@@ -1,4 +1,5 @@
 import { COMMON_CODE } from '@/error/code/common.code';
+import { HTTP_CODES } from '@/error/code/http.code';
 import { RATE_LIMIT_CODE } from '@/error/code/rate-limit.code';
 import { REDIS_CODE } from '@/error/code/redis.code';
 
@@ -6,6 +7,7 @@ export const ERROR_CODE: Record<string, string> = {
   ...REDIS_CODE,
   ...RATE_LIMIT_CODE,
   ...COMMON_CODE,
+  ...HTTP_CODES,
 } as const;
 
 export type ErrorCode = keyof typeof ERROR_CODE;
