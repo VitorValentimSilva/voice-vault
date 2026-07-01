8bf41385dd8d9aa58768ae9167bf6bc620a58d57
Vitor Valentim da Silva Ferreira <vitorvalentin840@gmail.com>
2026-07-01 20:37:13 -0300

    ✨ feat: enhance ESLint configuration and add Metro config for NativeWind

--- mobile/eslint.config.mjs
+++ mobile/eslint.config.mjs
@@ -1,13 +1,27 @@
 import expoConfig from 'eslint-config-expo/flat.js';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import simpleImportSort from 'eslint-plugin-simple-import-sort';
 import unusedImports from 'eslint-plugin-unused-imports';
-import { defineConfig } from 'eslint/config';
 import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
-export default defineConfig([
-  ...expoConfig,
+export default tseslint.config(
   {
+    ignores: ['node_modules/**', '.expo/**', 'dist/**', 'coverage/**'],
+  },
+  ...expoConfig,
+  eslintPluginPrettierRecommended,
+  {
+    files: ['eslint.config.mjs'],
+    rules: {
+      'import/no-named-as-default-member': 'off',
+    },
+  },
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [...tseslint.configs.recommendedTypeChecked],
     languageOptions: {
+      sourceType: 'module',
       parserOptions: {
         projectService: true,
         tsconfigRootDir: import.meta.dirname,
@@ -16,11 +30,17 @@ export default defineConfig([
         ...globals.node,
       },
     },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
     plugins: {
       'simple-import-sort': simpleImportSort,
       'unused-imports': unusedImports,
     },
     rules: {
+      'import/no-named-as-default-member': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
@@ -35,4 +55,13 @@ export default defineConfig([
       ],
     },
   },
-]);
+  {
+    files: ['**/*.{js,cjs,mjs}'],
+    languageOptions: {
+      sourceType: 'module',
+      globals: {
+        ...globals.node,
+      },
+    },
+  }
+);
