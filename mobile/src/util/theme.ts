67ce35abc126dd9ffa53dc2f94db737e94699008
Vitor Valentim da Silva Ferreira <vitorvalentin840@gmail.com>
2026-07-14 20:02:29 -0300

    fix 🐛: fix theme preference validation and update navigation themes accordingly
    Resolved issues with theme preference validation and updated navigation themes to reflect user preferences.

--- mobile/src/util/theme.ts
+++ mobile/src/util/theme.ts
@@ -1,6 +1,7 @@
+import { DarkTheme, DefaultTheme } from 'expo-router/react-navigation';
 import { Appearance } from 'react-native';
 
-import { ThemePreference } from '@/const/theme.const';
+import { THEME, ThemePreference } from '@/const/theme.const';
 
 export function resolveEffectiveTheme(
   preference: ThemePreference | undefined
@@ -11,3 +12,30 @@ export function resolveEffectiveTheme(
 
   return preference;
 }
+
+export const NAV_THEME = {
+  light: {
+    ...DefaultTheme,
+    colors: {
+      ...DefaultTheme.colors,
+      background: THEME.light.background,
+      border: THEME.light.border,
+      card: THEME.light.card,
+      notification: THEME.light.destructive,
+      primary: THEME.light.primary,
+      text: THEME.light.foreground,
+    },
+  },
+  dark: {
+    ...DarkTheme,
+    colors: {
+      ...DarkTheme.colors,
+      background: THEME.dark.background,
+      border: THEME.dark.border,
+      card: THEME.dark.card,
+      notification: THEME.dark.destructive,
+      primary: THEME.dark.primary,
+      text: THEME.dark.foreground,
+    },
+  },
+};
